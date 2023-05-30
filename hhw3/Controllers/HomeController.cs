fe1c4420843cd25fd2cb72655c09a87dc047f44d
Nasser Hadi <Naseerhadie@gmail.com>
2023-05-30 20:36:57 +0300

    first commit

---
+++ hhw3/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using hhw3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace hhw3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Rev(string Name)
+        {
+            var r = new string(Name.Reverse().ToArray());
+            ViewData["n"] = r;
+            return View("Index");
+        }
+
+      
+    }
+}
---
+++ hhw3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hhw3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
